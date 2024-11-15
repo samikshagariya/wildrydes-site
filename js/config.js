8fae969d6c17e52987f0e68a20ae34fd394517ae
samikshagariya <128296261+samikshagariya@users.noreply.github.com>
2024-11-15 08:43:54 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,9 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_jnI7MWtxH', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6jq9ifmkr0um9dbde5ht0s5c18
+', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
